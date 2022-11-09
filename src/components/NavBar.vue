2209c011edf6a34e2a2c50fb21ed76d3c23cfbd0
Me <sinon049@yahoo.com.tw>
2022-11-09 15:29:40 +0800

    Add navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Restaurant Drawer</a>
+    </div>
+  </nav>
+</template>
